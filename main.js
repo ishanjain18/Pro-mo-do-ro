1e29fd2431359b389ab77b29fd0dfb009313ce10
ishan jain <ishan10j@gmail.com>
2020-05-03 09:18:55 +0000

    updated

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const parallax= document.getElementById
+/*const parallax= document.getElementById
 ("big");
 window.addEventListener("scroll", function()
 {
@@ -7,3 +7,26 @@ window.addEventListener("scroll", function()
     * 0.7 +"px";
 
 })
+*/
+
+
+var t1=new TimelineMax({onUpdate:updatePercentage});
+const controller=new ScrollMagic.Controller();
+
+t1.from("p", .5, {x:200,opacity:0});
+t1.from('#homo1', 1, {x:200, opacity:0});
+t1.from('#homo2', 1, {x:200, opacity:0});
+const scene=new ScrollMagic.Scene({
+    triggerElement:"white",
+    triggerHook:"onLeave",
+    duration:"100%"
+})
+
+.setPin(".white")
+.setTween(t1)
+ .addTo(controller);
+
+ function updatePercentage() {
+ t1.progress();
+ console.log(t1.progress());
+ }
--- static/timer.js
+++ static/timer.js
@@ -1,37 +1,82 @@
 
 
-                        function timeractivate{{i}}(){
-                            var d = new Date();
-                            var countDownDate = d.getTime() + 1500000;
 
 
-                          // Update the count down every 1 second
-                            var x = setInterval(function() {
-
-                            // Get today's date and time
-                            var now = new Date().getTime();
-
-
-                            // Find the distance between now and the count down date
-                            var distance = countDownDate - now;
+                        function pomoactivate{{i}}()
+                         {
+                              var d = new Date();
+                              var countDownDate = d.getTime() + 1501000;
+                              var x;
+                              // Update the count down every 1 second
+                                  x = setInterval(function() {
 
 
-                            // Time calculations for days, hours, minutes and seconds
+                                      // Get today's date and time
+                                      var now = new Date().getTime();
 
-                            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-                            // Output the result in an element with id="timer"
-                            document.getElementById("timer{{i}}").innerHTML = minutes + "m " + seconds + "s ";
 
-                            // If the count down is over, write some text
-                            if (distance < 0) {
+                                      // Find the distance between now and the count down date
+                                      var distance = countDownDate - now;
+                                      document.getElementById("timer{{i}}").innerHTML = "STARTING...";
+
+
+                                      // Time calculations for days, hours, minutes and seconds
+
+                                      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                                      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                                      // Output the result in an element with id="timer"
+                                      document.getElementById("timer{{i}}").innerHTML = minutes + "m " + seconds + "s ";
+
+                                      // If the count down is over, write some text
+                                      if (distance < 0) {
+                                          clearInterval(x);
+                                          document.getElementById("timer{{i}}").innerHTML = "COMPLETED";
+                                          let src = '/static/sound.mp3';
+                                          let audio = new Audio(src);
+                                          audio.play();
+                                          alert("PomoDoro Complete! Start Another!")
+                                      }
+                                        }, 1000);
+                                    }
+
+
+                  function breakactivate{{i}}()
+                  {
+
+
+                      var d = new Date();
+                      var countDownDate = d.getTime() + 301000;
+
+
+                      // Update the count down every 1 second
+                      var x = setInterval(function() {
+
+                          // Get today's date and time
+                          var now = new Date().getTime();
+
+
+                          // Find the distance between now and the count down date
+                          var distance = countDownDate - now;
+
+
+                          // Time calculations for days, hours, minutes and seconds
+
+                          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                          // Output the result in an element with id="timer"
+                          document.getElementById("timer{{i}}").innerHTML = minutes + "m " + seconds + "s ";
+
+                          // If the count down is over, write some text
+                          if (distance < 0) {
                               clearInterval(x);
                               document.getElementById("timer{{i}}").innerHTML = "COMPLETED";
                               let src = '/static/sound.mp3';
                               let audio = new Audio(src);
                               audio.play();
-                              alert("PomoDoro Complete, Start Another!")
-                            }
-                          }, 1000);
-                        }
+                              alert("Break Complete!")
+                          }
+                      }, 1000);
+                  }
