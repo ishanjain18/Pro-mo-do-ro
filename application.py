f8bca5546cd1ef33ed96c42954fdf66b8750c770
ishan jain <ishan10j@gmail.com>
2020-05-05 20:02:40 +0000

    deploying..

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite3://users.db")
+db = SQL("sqlite:///users.db")
 
 @app.route("/")
 def index():
