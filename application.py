846e80b3f7679717f5da5a0514fe74e3c8663281
ishan jain <ishan10j@gmail.com>
2020-05-05 16:58:58 +0000

    deploying..

--- application.py
+++ application.py
@@ -180,5 +180,4 @@ for code in default_exceptions:
 
 if __name__ == '__main__':
  app.debug = True
- port = int(os.environ.get(“PORT”, 5000))
- app.run(host=’0.0.0.0', port=port)
+ app.run(host=’0.0.0.0', port=5000)
