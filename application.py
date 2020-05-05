8a8a9951421306dee266d9305d361477217756ca
ishan jain <ishan10j@gmail.com>
2020-05-05 15:06:50 +0000

    added Procfile

--- application.py
+++ application.py
@@ -175,6 +175,6 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 
-#if __name__ == '__main__':
- #   app.debug = True
-  #  app.run(host = '0.0.0.0', port = 5000)
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
