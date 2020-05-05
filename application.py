d2740235b11abdf86c12e949af25cbc446b11498
ishan jain <ishan10j@gmail.com>
2020-05-05 19:59:17 +0000

    deploying..

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("postgres://pwjdyffldbnkyw:88c8437d27352dfc5e4128c60929cdab961f02fed030886dd1cf13ccbd6b5a0a@ec2-54-88-130-244.compute-1.amazonaws.com:5432/d9eg0n6e5s9srl")
+db = SQL("sqlite3://users.db")
 
 @app.route("/")
 def index():
