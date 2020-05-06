4428b065ed2bdfb8aa4161099a2fef4697fa6f07
ishan jain <ishan10j@gmail.com>
2020-05-06 14:59:58 +0000

    deploying..

--- application.py
+++ application.py
@@ -101,8 +101,8 @@ def register():
         hsh = generate_password_hash(request.form.get("password"))
         r = random.randint(0, 124823734)
 
-        if db.execute("INSERT INTO users(id, username, hash) VALUES (:id1, :username, :hasher);", id1=r, username=request.form.get("username"), hasher=hsh):
-            return apology("inserted, but not really", 403)
+        if db.execute("INSERT INTO users(id, username, hash) VALUES (:id1, :username, :hasher);", id1=r, username=request.form.get("username"), hasher=hsh) == 0:
+            return apology("not inserting", 403)
 
 
         # Redirect user to login page
