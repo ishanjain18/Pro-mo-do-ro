b7d22ca73ff19c9301ace20ddf52d2cc25c1d8ed
ishan jain <ishan10j@gmail.com>
2020-05-06 15:53:15 +0000

    deploying..

--- application.py
+++ application.py
@@ -103,7 +103,7 @@ def register():
         r = random.randint(0, 124823734)
 
         db.execute("INSERT INTO users(id, username, hash) VALUES (:id1, :username, :hasher);", id1=r, username=request.form.get("username"), hasher=hsh)
-
+        db.execute("COMMIT;")
 
         # Redirect user to login page
         return redirect("/login")
